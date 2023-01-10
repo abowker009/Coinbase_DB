18a07bf3ec950fa3dcfc464f144bc5136bf1f2ff
abowker009 <116855995+abowker009@users.noreply.github.com>
2023-01-10 13:29:49 -0500

    Create update_crypto_db.py

---
+++ update_crypto_db.py
@@ -0,0 +1,25 @@
+from datetime import datetime, timedelta
+import pandas as pd
+
+product_list= ["BTC","ETH"]
+base_currency= ["USD"]
+
+#intervals 
+coinbase_interval= "ONE_MINUTE" #ONE_MINUTE,FIVE_MINUTE, FIFTEEN_MINUTE,THIRTY_MINUTE,ONE_HOUR,TWO_HOUR,SIX_HOUR,ONE_DAY
+
+
+
+
+# Get UTC time for the beginning of January 1, 2019
+start_date = datetime(year=2022, month=12, day=8, hour=0, minute=0, second=0, microsecond=0)
+
+
+# Get current time in UTC
+end_date = datetime.utcnow()
+
+# Subtract one day from UTC time
+end_date -= timedelta(days=1)
+# Set time to the last minute of the previous day
+end_date = end_date.replace(hour=23, minute=59, second=0, microsecond=0)
+# Convert UTC time to Unix time (seconds since Jan 1, 1970)
+date_range = pd.date_range(start_date, end_date,freq="300min")
